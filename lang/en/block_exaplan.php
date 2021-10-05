5c9e37c1015ce0815101db6f22396c2debcb2876
FabioPernegger <fabio.pernegger@gmail.com>
2021-10-05 12:16:36 +0200

    new block created

---
+++ lang/en/block_exaplan.php
@@ -0,0 +1,5 @@
+<?php
+$string['pluginname'] = 'Planningtool';
+$string['exaplan'] = 'Planningtool';
+$string['exaplan:addinstance'] = 'Add a new exapaln block';
+$string['exaplan:myaddinstance'] = 'Add a new exaplan block to the My Moodle page';
