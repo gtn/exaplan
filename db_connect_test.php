a926d4df210bca52059383f97806c78fab6c9263
Richard Wolfmayr <richard.wolfmayr@gmail.com>
2021-10-07 18:37:23 +0200

    Update db_connect_test.php

--- db_connect_test.php
+++ db_connect_test.php
@@ -1,10 +1,21 @@
 <?php
 // TODO: configdatei
+//$pdo = new PDO('mysql:host=localhost;dbname=moodle', 'root', '');
+//
+//$statement = $pdo->prepare("SELECT * FROM mdl_block_exacompexamples WHERE title = ?");
+//$statement->execute(array('Makros (LT)'));
+//$a = $statement->fetch();
+//
+//echo "done";
 
-$pdo = new PDO('mysql:host=localhost;dbname=moodle', 'root', '');
 
-$statement = $pdo->prepare("SELECT * FROM mdl_block_exacompexamples WHERE title = ?");
-$statement->execute(array('Makros (LT)'));
-$a = $statement->fetch();
+$pdo = new PDO('mysql:host=localhost;dbname=moodle2', 'root', '');
+
+$statement = $pdo->prepare("SELECT * FROM mdl_block_exaplanmodulesets");
+$statement->execute(array());
+$modulesets = $statement->fetchAll();
+foreach ($modulesets as $moduleset){
+    echo $moduleset["title"].'\n';
+}
 
 echo "done";
