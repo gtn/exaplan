ce95be7313c54d681d3125a00d3ab6ff6b789cec
Richard Wolfmayr <richard.wolfmayr@gmail.com>
2021-10-07 13:04:26 +0200

    started lib

--- lib/lib.php
+++ lib/lib.php
@@ -17,24 +17,139 @@
 //
 // This copyright notice MUST APPEAR in all copies of the script!
 
+defined('MOODLE_INTERNAL') || die();
 
-function block_exaplan_get_modulesets_by_puserid($puserid){
+/**
+ * DATABSE TABLE NAMES
+ */
+const BLOCK_EXAPLAN_DB_MODULESETS = 'block_exaplanmodulesets';
+
+/**
+ * @param $title
+ * @param $description
+ * @param $trainerpuserid
+ * @param $location
+ * @param $courseidnumber
+ * @return bool|int
+ * @throws dml_exception
+ */
+function block_exaplan_create_moduleset($title, $description, $trainerpuserid = null, $location = null, $courseidnumber = null)
+{
+    global $DB;
+
+    $date = new stdClass();
+    $date->title = $title;
+    $date->description = $description;
+    $date->trainerpuserid = $trainerpuserid;
+    $date->location = $location;
+    $date->courseidnumber = $courseidnumber;
+
+    return $DB->insert_record(BLOCK_EXAPLAN_DB_MODULESETS, $date);
+}
+
+/**
+ * @param $modulesetid
+ * @param $title
+ * @param $description
+ * @param $trainerpuserid
+ * @param $location
+ * @param $courseidnumber
+ * @return bool
+ * @throws dml_exception
+ */
+function block_exaplan_update_moduleset($modulesetid, $title = null, $description = null, $trainerpuserid = null, $location = null, $courseidnumber = null)
+{
+    global $DB;
+
+    $moduleset = $DB->get_record(BLOCK_EXAPLAN_DB_MODULESETS, array('id' => $modulesetid));
+    if ($title) $moduleset->title = $title;
+    if ($description) $moduleset->description = $description;
+    if ($trainerpuserid) $moduleset->trainerpuserid = $trainerpuserid;
+    if ($location) $moduleset->location = $location;
+    if ($courseidnumber) $moduleset->courseidnumber = $courseidnumber;
+
+    return $DB->update_record(BLOCK_EXAPLAN_DB_MODULESETS, $moduleset);
+}
+
+
+/**
+ * update title, description or subjectid of crosssubject
+ * @param unknown $crosssubjid
+ * @param unknown $title
+ * @param unknown $description
+ * @param unknown $subjectid
+ */
+function block_exacomp_edit_crosssub($crosssubjid, $title, $description, $subjectid, $groupcategory = "")
+{
+    global $DB;
+
+    $crosssubj = $DB->get_record(BLOCK_EXACOMP_DB_CROSSSUBJECTS, array('id' => $crosssubjid));
+    $crosssubj->title = $title;
+    $crosssubj->description = $description;
+    $crosssubj->subjectid = $subjectid;
+    $crosssubj->groupcategory = $groupcategory;
+
+    return $DB->update_record(BLOCK_EXACOMP_DB_CROSSSUBJECTS, $crosssubj);
+}
+
+/**
+ * remove given crosssubject
+ * @param unknown $crosssubjid
+ */
+function block_exacomp_delete_crosssub($crosssubjid)
+{
+    global $DB;
+    //delete examples that were created specifically only for this cross_subject
+    block_exacomp_delete_examples_for_crosssubject($crosssubjid);
+
+    // TODO: pruefen ob mein crosssubj?
+
+    //delete student-crosssubject association
+    $DB->delete_records(BLOCK_EXACOMP_DB_CROSSSTUD, array('crosssubjid' => $crosssubjid));
+
+    //delete descriptor-crosssubject association
+    $DB->delete_records(BLOCK_EXACOMP_DB_DESCCROSS, array('crosssubjid' => $crosssubjid));
+
+    //delete crosssubject overall evaluations
+    $DB->delete_records(BLOCK_EXACOMP_DB_COMPETENCES, array('compid' => $crosssubjid, 'comptype' => BLOCK_EXACOMP_TYPE_CROSSSUB));
+
+    //delete crosssubject
+    $DB->delete_records(BLOCK_EXACOMP_DB_CROSSSUBJECTS, array('id' => $crosssubjid));
+}
+
+
+function block_exaplan_get_modulesets_by_puserid($puserid)
+{
 
 }
 
-function block_exaplan_create_date(){
+function block_exaplan_create_date()
+{
 
 }
+
+function block_exaplan_update_date($dateid)
+{
+
+}
+
+function block_exaplan_delete_date()
+{
+
+}
+
 
 //function block_exaplan_get_trainers... by company??(){
 //
 //}
 
-function block_exaplan_get_(){
+function block_exaplan_get_()
+{
 
 }
 
-function block_exaplan_(){
+function block_exaplan_()
+{
 
 }
 
---
+++ test.php
@@ -0,0 +1,15 @@
+<?php
+require_once __DIR__."/inc.php";
+//block_exaplan_create_moduleset("all set", "description", "1", "location", "1");
+//block_exaplan_create_moduleset("minimum set", "description");
+//block_exaplan_create_moduleset("missing fields");
+
+
+
+block_exaplan_update_moduleset(8, "newTitle2");
+
+
+
+
+
+echo "done";
