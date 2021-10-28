ba18363636ebb3bdea1c8ab9f808e0c4eae1c561
Richard Wolfmayr <richard.wolfmayr@gmail.com>
2021-10-28 10:47:37 +0200

    gitignore

--- lang/en/block_exaplan.php
+++ lang/en/block_exaplan.php
@@ -1,6 +1,6 @@
 <?php
-$string['pluginname'] = 'Planning Tool';
-$string['exaplan'] = 'Planning Tool';
+$string['pluginname'] = 'Exabis Planning Tool';
+$string['exaplan'] = 'Exabis Planning Tool';
 $string['exaplan:addinstance'] = 'Add a new planning tool block';
 $string['exaplan:myaddinstance'] = 'Add a new planning tool block to the My Moodle page';
 $string['messageprovider:datefixed'] = 'Date has been fixed';
